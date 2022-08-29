6662e2725d46387252ca5ceb259fff0255fe37fb
Shubham Kumar <shubhamkumarlhh@gmail.com>
2022-08-29 22:23:57 +0530

    initial commit

--- src/app.js
+++ src/app.js
@@ -11,5 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 app.use(contestRouter);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 module.exports = app;
